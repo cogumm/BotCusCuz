bc071c274db388c6dd10bf685abb1d537f918d97
Gabriel Feitosa Vilar <cogumm@gmail.com>
2011-06-14 02:38:16 -0300

    Mudando apra sinatra

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bot::Application.config.secret_token = '06e0ea1f98b656398ea2b6f3368203a6ae08c300aa4a53eda5f4dc99c3135659c2663c89f60af81552362c098b40faa5ddd61bd2be993453bfc7a4599999cb76'
